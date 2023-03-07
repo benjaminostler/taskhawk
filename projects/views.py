993a86860481db84d4c69abdea6e0f4beefd7e9c
Benjamin Ostler <benjaminostler@gmail.com>
2023-03-07 13:47:26 -0700

    Feature 14 complete

--- projects/views.py
+++ projects/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import (
 )
 from projects.models import Project
 from tasks.models import Task
+from projects.forms import ProjectForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -27,3 +28,27 @@ def show_project(request, id):
         "task_list": task_list,
     }
     return render(request, "projects/detail.html", context)
+
+
+@login_required
+def create_project(request):
+    # Check if request is a POST
+    if request.method == "POST":
+        # Create new form instance & fill with values from submission
+        form = ProjectForm(request.POST)
+        # Check if the form is valid
+        if form.is_valid():
+            # save all info from form into variable
+            project = form.save(False)
+            # add logged in user as the purchaser attribute in receipt
+            project.owner = request.user
+            # Save receipt with user info
+            project.save()
+        # Redirect to a different page
+        return redirect("list_projects")
+    else:
+        # Create a new form instance
+        form = ProjectForm()
+    # Add it to the context
+    context = {"form": form}
+    return render(request, "projects/create.html", context)
